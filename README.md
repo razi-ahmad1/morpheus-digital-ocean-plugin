1b3de1aad69b7a32b54e5144d8fbdcc6421d6dee
Dustin Deyoung <ddeyoung@morpheusdata.com>
2023-03-16 13:57:27 -0400

    Initial export to external repo.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# morpheus-digital-ocean-plugin
+## Morpheus DigitalOcean Plugin
+
+This is the official Morpheus plugin for interacting with the Digital Ocean API. This plugin provides droplet provisioning, snapshot create, and snapshot restore.
+
+### Building
+
+This is a Morpheus plugin that leverages the `morpheus-plugin-core` which can be referenced by visiting [https://developer.morpheusdata.com](https://developer.morpheusdata.com). It is a groovy plugin designed to be uploaded into a Morpheus environment via the `Administration -> Integrations -> Plugins` section. To build this product from scratch simply run the shadowJar gradle task on java 11:
+
+```bash
+./gradlew shadowJar
+```
+
+A jar will be produced in the `build/lib` folder that can be uploaded into a Morpheus environment.
+
+
+### Configuring
+
+Once the plugin is loaded in the environment. DigitalOcean becomes available as an option when creating a new cloud in `Infrastructure -> Clouds`.
+
+The following information is required when adding a DigitalOcean cloud:
+
+1. Username: Your DigitalOcean username.
+1. API Key: A personal access token generated on the DigitalOcean portal.
+3. Datacenter: A list of available datacenters is provided.
+
---
+++ src/main/groovy/com/morpheusdata/digitalocean/backup/DigitalOceanBackupProvider.groovy
@@ -0,0 +1,34 @@
+package com.morpheusdata.digitalocean.backup
+
+import com.morpheusdata.digitalocean.DigitalOceanPlugin
+import com.morpheusdata.digitalocean.DigitalOceanApiService
+import com.morpheusdata.digitalocean.backup.DigitalOceanSnapshotProvider
+import com.morpheusdata.core.backup.BackupExecutionProvider
+import com.morpheusdata.core.backup.BackupJobProvider
+import com.morpheusdata.core.backup.BackupProvider;
+import com.morpheusdata.core.MorpheusContext
+import com.morpheusdata.core.Plugin
+import com.morpheusdata.core.backup.BackupRestoreProvider
+import com.morpheusdata.core.backup.BackupTypeProvider
+import com.morpheusdata.core.backup.MorpheusBackupProvider
+import com.morpheusdata.model.BackupIntegration
+import com.morpheusdata.model.BackupJob
+import com.morpheusdata.model.BackupProviderType
+import com.morpheusdata.model.BackupType
+import com.morpheusdata.model.Icon
+import com.morpheusdata.model.OptionType
+import com.morpheusdata.model.ReplicationType
+import com.morpheusdata.response.ServiceResponse
+
+class DigitalOceanBackupProvider extends MorpheusBackupProvider {
+
+	DigitalOceanBackupProvider(Plugin plugin, MorpheusContext morpheusContext) {
+		super(plugin, morpheusContext)
+
+		DigitalOceanSnapshotProvider digitalOceanSnapshotProvider = new DigitalOceanSnapshotProvider(plugin, morpheusContext)
+		plugin.pluginProviders.put(digitalOceanSnapshotProvider.code, digitalOceanSnapshotProvider)
+		addScopedProvider(digitalOceanSnapshotProvider, "do-provider", null)
+	}
+
+
+}
